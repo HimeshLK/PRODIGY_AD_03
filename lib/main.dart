30047b93428e06b055f78678da8a5909c74b1d85
Himesh Fernando <himeshfernando.it@gmail.com>
2023-12-26 20:19:38 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Stopwatch',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(),
+        debugShowCheckedModeBanner: false);
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Stopwatch stopwatch;
+  late Timer t;
+
+  void handleStartStop() {
+    if (stopwatch.isRunning) {
+      stopwatch.stop();
+    } else {
+      stopwatch.start();
+    }
+  }
+
+  String returnFormattedText() {
+    var milli = stopwatch.elapsed.inMilliseconds;
+
+    String milliseconds =
+        (milli % 1000).toString().padLeft(3, "0"); //miliseconds
+    String seconds =
+        ((milli ~/ 1000) % 60).toString().padLeft(2, "0"); // second
+    String minutes =
+        ((milli ~/ 1000) ~/ 60).toString().padLeft(2, "0"); // minute
+
+    return "$minutes:$seconds:$milliseconds";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    stopwatch = Stopwatch();
+
+    t = Timer.periodic(const Duration(milliseconds: 30), (timer) {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            // this is the column
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CupertinoButton(
+                onPressed: () {
+                  handleStartStop();
+                },
+                padding: const EdgeInsets.all(0),
+                child: Container(
+                  height: 250,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle, //  circle on ui.
+                    border: Border.all(
+                      color: const Color(0xff0395eb),
+                      width: 4,
+                    ),
+                  ),
+                  child: Text(
+                    returnFormattedText(),
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CupertinoButton(
+                // Buttonj For reset button function
+                onPressed: () {
+                  stopwatch.reset();
+                },
+                padding: const EdgeInsets.all(0),
+                child: const Text(
+                  "Reset",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
